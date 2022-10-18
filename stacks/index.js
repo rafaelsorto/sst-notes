6a9b689fefd017ffdca997fcd9d4ee4035f9e258
Rafael Sorto <rafael@analogrepublic.com>
2022-10-18 10:36:29 -0600

    Added Storage Stack

---
+++ stacks/index.js
@@ -0,0 +1,16 @@
+import { App } from "@serverless-stack/resources";
+import { StorageStack } from "./StorageStack";
+
+/**
+ * @param {App} app
+ */
+export default function (app) {
+  app.setDefaultFunctionProps({
+    runtime: "nodejs16.x",
+    srcPath: "services",
+    bundle: {
+      format: "esm",
+    },
+  });
+  app.stack(StorageStack);
+}
